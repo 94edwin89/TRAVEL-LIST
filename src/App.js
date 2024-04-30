fda53b392fad771495987b9d65e79d9887e96f6a
Edwin Jude <edwin.judefrancis@gmail.com>
2024-04-30 18:59:59 +0530

    about State and lifting State Up

--- src/App.js
+++ src/App.js
@@ -7,11 +7,17 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] =useState([])
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item])
+  }
+
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -21,20 +27,21 @@ function Logo() {
   return <h1>🌴 Far Away 🧳</h1>;
 }
 
-function Form() {
+function Form({onAddItems}) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
+
 
   function handleSubmit(e) {
     e.preventDefault();
     if (!description) return;
 
     const newItem = {
-      description,
-      quantity,
-      packed: false,
-      id: Date.now(),
+      description,quantity,packed: false,id: Date.now(),
     };
+
+    onAddItems(newItem);
+
     setDescription('')
     setQuantity(1);
   }
@@ -65,11 +72,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({items}) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
@@ -81,7 +88,7 @@ function Item({ item }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
-        {item.quantity}
+        {item.quantity }
         {item.description}
       </span>
       <button>❌</button>
@@ -96,3 +103,4 @@ function Stats() {
     </footer>
   );
 }
+ 
