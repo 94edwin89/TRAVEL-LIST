d78f9253c712f69a139788dcf8c8b0580e216284
Edwin Jude <edwin.judefrancis@gmail.com>
2024-04-30 23:34:37 +0530

    deleteing item more child to parent communication

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import { useState } from "react";
 
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: true },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Charger", quantity: 1, packed: true },
-];
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: true },
+//   { id: 2, description: "Socks", quantity: 12, packed: false },
+//   { id: 3, description: "Charger", quantity: 1, packed: true },
+// ];
 
 export default function App() {
-  const [items, setItems] =useState([])
+  const [items, setItems] = useState([]);
 
   function handleAddItems(item) {
-    setItems((items) => [...items, item])
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items
+      .filter((item) => item.id !== id));
   }
 
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem} />
       <Stats />
     </div>
   );
@@ -27,22 +32,24 @@ function Logo() {
   return <h1>🌴 Far Away 🧳</h1>;
 }
 
-function Form({onAddItems}) {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
-
 
   function handleSubmit(e) {
     e.preventDefault();
     if (!description) return;
 
     const newItem = {
-      description,quantity,packed: false,id: Date.now(),
+      description,
+      quantity,
+      packed: false,
+      id: Date.now(),
     };
 
     onAddItems(newItem);
 
-    setDescription('')
+    setDescription("");
     setQuantity(1);
   }
 
@@ -72,26 +79,26 @@ function Form({onAddItems}) {
   );
 }
 
-function PackingList({items}) {
+function PackingList({ items, onDeleteItem }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item item={item} key={item.id} />
+          <Item item={item} onDeleteItem={onDeleteItem} key={item.id} />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
-        {item.quantity }
+        {item.quantity}
         {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
@@ -103,4 +110,3 @@ function Stats() {
     </footer>
   );
 }
- 
